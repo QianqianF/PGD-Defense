4f175baa1bbe97a5058fef0e049f6cc7fefac43c
Jannis B <jannis.bolik@student.hpi.de>
2021-11-05 08:56:54 +0100

    Add regression PGD attack

--- pgd.py
+++ pgd.py
@@ -27,7 +27,27 @@ def fgsm_(model_, device, x, target, eps, targeted=True, clip_min=None, clip_max
     return out
 
 
-def pgd_(model_, device, x, target, k, eps, eps_step, targeted=True, clip_min=None, clip_max=None, random_start=False):
+# minimizes regression output
+def fgsm_regression_(model_, device, x, eps, clip_min=None, clip_max=None):
+    """Internal process for all FGSM and PGD attacks."""
+    # create a copy of the input, remove all previous associations to the compute graph...
+    input_ = x.clone().detach_()
+    # ... and make sure we are differentiating toward that variable
+    input_.requires_grad_()
+
+    # run the model and obtain the loss
+    _, loss = model_(input_)
+    loss.backward()
+
+    out = input_ - eps * input_.grad.sign()
+
+    # if desired clip the ouput back to the image domain
+    if (clip_min is not None) or (clip_max is not None):
+        out.clamp_(min=clip_min, max=clip_max)
+    return out
+
+
+def pgd_(model_, device, x, target, k, eps, eps_step, targeted=True, clip_min=None, clip_max=None, random_start=False, regression=False):
     x = x.clone().detach()
 
     x_min = x - eps
@@ -44,7 +64,10 @@ def pgd_(model_, device, x, target, k, eps, eps_step, targeted=True, clip_min=No
         # FGSM step
         # We don't clamp here (arguments clip_min=None, clip_max=None)
         # as we want to apply the attack as defined
-        x = fgsm_(model_, device, x, target, eps_step, targeted)
+        if regression:
+            x = fgsm_regression_(model_, device, x, eps)
+        else:
+            x = fgsm_(model_, device, x, target, eps_step, targeted)
         # Projection Step
         x = torch.max(x_min, x)
         x = torch.min(x_max, x)
