e1e13fa888356d34dc24e890787fea5a84d782fd
Jannis B <jannis.bolik@student.hpi.de>
2021-12-17 15:54:35 +0100

    Add certified volumes metric to compare_certifications.py

--- code/compare_certifications.py
+++ code/compare_certifications.py
@@ -18,18 +18,26 @@ correct0 = 0
 correct1 = 0
 correct_both = 0
 relative_radius_sum = 0.
+certified_volume0 = 0.
+certified_volume1 = 0.
 for i in range(limit):
     if columns0[4][i] == '1':
         correct0 += 1
+        certified_volume0 += float(columns0[3][i]) ^ (32 * 32 * 3)
     if columns1[4][i] == '1':
         correct1 += 1
+        certified_volume1 += float(columns1[3][i]) ^ (32 * 32 * 3)
     if columns0[4][i] == '1' and columns1[4][i] == '1':
         correct_both += 1
         relative_radius = float(columns1[3][i]) / float(columns0[3][i])
         print(relative_radius)
         relative_radius_sum += relative_radius
 
+print('all statistics are for the images which were (tried to be) certified in both files')
 print('correctly classified images in file 0:', correct0)
 print('correctly classified images in file 1:', correct1)
 print('correctly classified images in both files:', correct_both)
 print('mean relative increase in certified radius:', relative_radius_sum / correct_both)
+print('certified volume in file 0:', certified_volume0)
+print('certified volume in file 1:', certified_volume1)
+print('relative certified volume:', certified_volume1 / certified_volume0)
