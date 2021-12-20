116477e1cee42e133a827e789ba7bf276845b19d
Jannis B <jannis.bolik@student.hpi.de>
2021-12-20 13:58:55 +0100

    Try to fix l2 norm statistic

--- code/intermediate.py
+++ code/intermediate.py
@@ -27,12 +27,12 @@ class Intermediate(nn.Module):
 
     def forward(self, batch, noise):
         print('initial stddev:', math.sqrt(torch.mean(noise * noise).cpu().item()), file=self.outfile, flush=True)
-        print('initial mean l2 norm:', torch.mean(torch.norm(noise, dim=1)).cpu().item(), file=self.outfile, flush=True)
+        print('initial mean l2 norm:', torch.mean(torch.linalg.norm(noise.view(batch.shape[0], -1), dim=1)).cpu().item(), file=self.outfile, flush=True)
         x = batch + noise
         x = torch.clip(x, 0., 1.)
         attacker = PGD_L2(steps=1, device='cuda', max_norm=self.epsilon / self.num_steps)
         for i in range(self.num_steps):
             x = attacker.attack(self.base_classifier, x, None, entropy_attack=True, entropy_samples=self.entropy_samples)
             print('stddev after PGD step ' + str(i) + ':', math.sqrt(torch.mean((x - batch) * (x - batch)).cpu().item()), file=self.outfile, flush=True)
-            print('mean l2 norm after PGD step ' + str(i) + ':', torch.mean(torch.norm(x - batch, dim=1)).cpu().item(), file=self.outfile, flush=True)
+            print('mean l2 norm after PGD step ' + str(i) + ':', torch.mean(torch.linalg.norm((x - batch).view(batch.shape[0], -1), dim=1)).cpu().item(), file=self.outfile, flush=True)
         return self.base_classifier(x)
