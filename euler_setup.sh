ed793b08b6fe7286f2980063ca92182c45f75af2
QianqianF <tiffanyqqfeng@gmail.com>
2021-12-27 23:55:48 +0100

    config vim to develop on cluster, add plotting code for entropy comparison

--- euler_setup.sh
+++ euler_setup.sh
@@ -1,5 +1,6 @@
 env2lmod
 module load gcc/8.2.0
 module load python_gpu/3.8.5
+module load vim/8.2.1201
 pip install git+https://github.com/piEsposito/blitz-bayesian-deep-learning.git
 pip install wandb
