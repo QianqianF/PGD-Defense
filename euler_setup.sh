0cf41326c3e0b736157f7dcfd611430834a379f3
Jannis Bolik <jbolik@eu-login-20.euler.ethz.ch>
2021-12-29 16:49:07 +0100

    Update Python to 3.9

--- euler_setup.sh
+++ euler_setup.sh
@@ -1,5 +1,5 @@
 env2lmod
 module load gcc/8.2.0
-module load python_gpu/3.8.5
+module load python_gpu/3.9.9
 pip install git+https://github.com/piEsposito/blitz-bayesian-deep-learning.git
 pip install wandb
