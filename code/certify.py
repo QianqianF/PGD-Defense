8cbc8a34e23a18668947fc335551a1e3875895ba
Jannis B <jannis.bolik@student.hpi.de>
2022-01-08 17:38:03 +0100

    Add intermediate layer parameter grid search

--- code/certify.py
+++ code/certify.py
@@ -29,6 +29,7 @@ parser.add_argument('--use-intermediate', action='store_true', help="use interme
 parser.add_argument("--pgd-steps", type=int, default=5, help="number of PGD steps in intermediate layer")
 parser.add_argument("--pgd-epsilon", type=float, default=0.1, help="maximum l2 perturbation applied to samples in the intermediate layer")
 parser.add_argument("--pgd-bnn-samples", type=int, default=1, help="BNN samples to take at each step of the entropy attack (leave at 1 for plain NNs)")
+parser.add_argument("--pgd-tune", action='store_true', help="try out different steps and epsilons")
 
 parser.add_argument("--profile", action='store_true')
 args = parser.parse_args()
@@ -39,47 +40,60 @@ if __name__ == "__main__":
     base_classifier = get_architecture(checkpoint["arch"], args.dataset)
     base_classifier.load_state_dict(checkpoint['state_dict'])
 
-    # create the smooothed classifier
-    if args.use_intermediate:
-        smoothed_classifier = Smooth(Intermediate(base_classifier, args.pgd_steps, args.pgd_epsilon, args.pgd_bnn_samples), get_num_classes(args.dataset), args.sigma)
+    if args.pgd_tune:
+        l_pgd_steps = [1, 2, 3, 5, 10]
+        l_pgd_epsilon = [0.01, 0.1, 1.0, 7.0]
     else:
-        smoothed_classifier = Smooth(base_classifier, get_num_classes(args.dataset), args.sigma)
+        l_pgd_steps = [args.pgd_steps]
+        l_pgd_epsilon = [args.pgd_epsilon]
 
     # prepare output file
     f = open(args.outfile, 'w')
-    if not args.profile:
-        print("idx\tlabel\tpredict\tradius\tcorrect\ttime", file=f, flush=True)
 
-    # iterate through the dataset
-    dataset = get_dataset(args.dataset, args.split)
-    for i in range(len(dataset)):
+    for pgd_steps in l_pgd_steps:
+        for pgd_epsilon in l_pgd_epsilon:
 
-        # only certify every args.skip examples, and stop after args.max examples
-        if i % args.skip != 0:
-            continue
-        if i == args.max:
-            break
-
-        (x, label) = dataset[i]
-        x = x.cuda()
-
-        if args.profile:
-            print("starting profiling...")
-            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
-                with record_function("model_certification"):
-                    smoothed_classifier.certify(x, args.N0, args.N, args.alpha, args.batch)
+            # create the smooothed classifier
+            if args.use_intermediate:
+                smoothed_classifier = Smooth(Intermediate(base_classifier, pgd_steps, pgd_epsilon, args.pgd_bnn_samples), get_num_classes(args.dataset), args.sigma)
+            else:
+                smoothed_classifier = Smooth(base_classifier, get_num_classes(args.dataset), args.sigma)
             
-            print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=50))
-            break
-                
-        before_time = time()
-        # certify the prediction of g around x
-        prediction, radius = smoothed_classifier.certify(x, args.N0, args.N, args.alpha, args.batch)
-        after_time = time()
-        correct = int(prediction == label)
+            if args.pgd_tune:
+                print("pgd-steps: {}\tpgd-epsilon: {}".format(pgd_steps, pgd_epsilon), file=f, flush=True)
+            if not args.profile:
+                print("idx\tlabel\tpredict\tradius\tcorrect\ttime", file=f, flush=True)
 
-        time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
-        print("{}\t{}\t{}\t{:.3}\t{}\t{}".format(
-            i, label, prediction, radius, correct, time_elapsed), file=f, flush=True)
+            # iterate through the dataset
+            dataset = get_dataset(args.dataset, args.split)
+            for i in range(len(dataset)):
+
+                # only certify every args.skip examples, and stop after args.max examples
+                if i % args.skip != 0:
+                    continue
+                if i == args.max:
+                    break
+
+                (x, label) = dataset[i]
+                x = x.cuda()
+
+                if args.profile:
+                    print("starting profiling...")
+                    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
+                        with record_function("model_certification"):
+                            smoothed_classifier.certify(x, args.N0, args.N, args.alpha, args.batch)
+                    
+                    print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=50))
+                    break
+                        
+                before_time = time()
+                # certify the prediction of g around x
+                prediction, radius = smoothed_classifier.certify(x, args.N0, args.N, args.alpha, args.batch)
+                after_time = time()
+                correct = int(prediction == label)
+
+                time_elapsed = str(datetime.timedelta(seconds=(after_time - before_time)))
+                print("{}\t{}\t{}\t{:.3}\t{}\t{}".format(
+                    i, label, prediction, radius, correct, time_elapsed), file=f, flush=True)
 
     f.close()
