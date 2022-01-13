57df235b5ee2d8092f54787a794a31654b875640
QianqianF <tiffanyqqfeng@gmail.com>
2022-01-13 19:05:20 +0100

    compare entropy before and after intermediate

--- code/compare_entropy.py
+++ code/compare_entropy.py
@@ -29,12 +29,24 @@ args = parser.parse_args()
 def entropy(class_probabilities):
     return -torch.sum(torch.nan_to_num(class_probabilities * torch.log2(class_probabilities)), dim=1)
 
+def plot_entropy(results, plotname):
+    entropy_table = pd.DataFrame.from_records(results)
+    entropy_table['clean_entropy'] = entropy_table['clean_entropy'].transform(lambda t: t.item())
+    entropy_table['mean_noise_entropy'] = entropy_table['noise_entropy']\
+                                      .transform(lambda l: np.array([t.cpu() for t in l]).mean())
+    entropy_table['diff'] = entropy_table['diff'].transform(lambda t: t.item())
+    plot = entropy_table[['clean_entropy', 'mean_noise_entropy']].plot(kind='hist', bins=20, \
+                                                            alpha=0.7, color=['#A0E8AF', '#FFCF56'])
+    fig = plot.get_figure()
+    fig.savefig(os.path.join(args.outdir, plotname))
+
 
 if __name__ == "__main__":
     # load the base classifier
     checkpoint = torch.load(args.base_classifier)
     base_classifier = get_architecture(checkpoint["arch"], args.dataset)
     base_classifier.load_state_dict(checkpoint['state_dict'])
+    classifier = Intermediate(base_classifier=base_classifier, num_steps=1)
 
     # create the test dataset
     # train_dataset = get_dataset(args.dataset, 'train')
@@ -50,6 +62,7 @@ if __name__ == "__main__":
     
     os.makedirs(args.outdir, exist_ok = True)
     results = []
+    results_after = []
     f = open(os.path.join(args.outdir, "entropy.txt"), 'w+')
     print("idx\tclean_entropy\tnoise_entropy\tdiff", file=f, flush=True)
     for i in range(len(test_dataset)):
@@ -72,35 +85,31 @@ if __name__ == "__main__":
             else:
                 logits = base_classifier(batch)
                 class_probabilities = F.softmax(logits, 1)
-            #print(logits.shape)
-            #clean_entropy = entropy(logits[0])
 
-            #noise_entropy = torch.zeros(args.sample_size)
-            #for i in range(args.sample_size):
-            #    noise_entropy[i] = entropy(logits[i+1])
-            
             batch_entropy = entropy(class_probabilities)
             clean_entropy = batch_entropy[0]
             noise_entropy = batch_entropy[1:]
             diff = (noise_entropy - clean_entropy).mean()
 
+            _, class_probabilities_after = classifier(batch)
+            batch_entropy_after = entropy(class_probabilities_after)
+            clean_entropy_after = batch_entropy[0]
+            noise_entropy_after = batch_entropy[1:]
+            diff_after = (clean_entropy_after - noise_entropy_after).mean()
+            
+
             print("{}\t{}\t{}\t{}".format(i, clean_entropy, noise_entropy, diff), file=f, flush=True)
             res = {"clean_entropy": clean_entropy, "noise_entropy": noise_entropy, "diff": diff}
+            res_after = {"clean_entropy": clean_entropy, "noise_entropy": noise_entropy, "diff": diff}
             results.append(res)
+            results_after.append(res_after)
 
     np.save(os.path.join(args.outdir, "entropy.npy"), results)
-
+    np.save(os.path.join(args.outdir, "entropy_after.npy"), results_after)
     
     # Plotting
-    entropy_table = pd.DataFrame.from_records(results)
-    entropy_table['clean_entropy'] = entropy_table['clean_entropy'].transform(lambda t: t.item())
-    entropy_table['mean_noise_entropy'] = entropy_table['noise_entropy']\
-                                      .transform(lambda l: np.array([t.cpu() for t in l]).mean())
-    entropy_table['diff'] = entropy_table['diff'].transform(lambda t: t.item())
-    plot = entropy_table[['clean_entropy', 'mean_noise_entropy']].plot(kind='hist', bins=20, \
-                                                            alpha=0.7, color=['#A0E8AF', '#FFCF56'])
-    fig = plot.get_figure()
-    fig.savefig(os.path.join(args.outdir, "clean_vs_mean_entropy.png"))
+    plot_entropy(results, "clean_vs_mean_entropy.png")
+    plot_entropy(results_after, "clean_vs_mean_entropy_after.png")
     
         
 
